323cb511aeecdd147f78555e6825c099a85a6c21
hanumanth <hanumanthumandula@gmail.com>
2026-06-07 21:21:08 +0530

    Fix MongoDB connection timeout

--- server.js
+++ server.js
@@ -6,11 +6,18 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+})
   .then(() => console.log('MongoDB Connected Successfully'))
   .catch(err => console.log('MongoDB Error:', err));
 
